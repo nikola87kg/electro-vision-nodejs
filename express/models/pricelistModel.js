62e277f056ae46b4ceb95c258999d3d69d459a15
Nikola Jeremic <njeremic@lioneight.com>
2020-05-01 20:26:07 +0200

    pricelist improvement

--- express/models/pricelistModel.js
+++ express/models/pricelistModel.js
@@ -1,17 +1,18 @@
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 /* Creates a schema */
-var pricelistSchema = new Schema({
-    name:           {type: String, required: true, trim: true, unique: true},
-    description:    {type: String, trim: true, default: 'Ova stavka cenovnika nema opis'},
-    price:          {type: String, required: true }
-}, {timestamps: true});
+var pricelistSchema = new Schema(
+	{
+		name: { type: String, required: true, trim: true, unique: true },
+		priceGroup: { type: Object },
+		price: { type: String, required: true }
+	},
+	{ timestamps: true }
+);
 
 /* Creates a model */
 const PricelistModel = mongoose.model('Pricelist', pricelistSchema);
-
 
 /* Export */
 module.exports = PricelistModel;
